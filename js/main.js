ca64d80470d907a04bccf75cb9f707ebd972d54f
Thierry GRELL <thigre.sspi@gmail.com>
2025-06-16 09:54:25 +0200

    -- update

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
-console.log('Lauched !');
-
-const addBtn = document.querySelector('#btn');
+//déclaration de constantes
+const addBtn = document.querySelector('#btn'); // retourne le premier élément du DOM qui coïncide avec le selecteur CSS ( id='btn' dans ce cas)
 const taskCard = document.querySelector(".todoCard");
 const tasksContainer = document.querySelector('#todoCards');
-const delBtn = document.querySelector('.delBtn')
+const delBtn = document.querySelector('.delBtn');
 
+//events Listener
 addBtn.addEventListener('click', addTask); // add a task on a click
 delBtn.addEventListener('click', function() {deleteTask(taskCard);});
 
-function addTask(){
-    const newTask = taskCard.cloneNode(true);
+
+//mise en forme première liste
+updateTaskName(taskCard);
+
+//déclaration de fonctions
+function addTask(){         //fonction qui permet d'ajouter une tâche dans la liste
+    const newTask = taskCard.cloneNode(true); // clone l'objet taskCard (élément .todoCard)
     const newDelBtn = newTask.querySelector('.delBtn');
-    const newTextArea = newTask.querySelector('.task');
 
-    newTextArea.value = "New Task";
     newDelBtn.addEventListener('click', function(){deleteTask(newTask);});
-
     tasksContainer.appendChild(newTask);
-    //updateCount();
+    updateTaskName(newTask);
 }
 
-function deleteTask(task){
+function deleteTask(task){  //fonction qui permet de supprimer une tâche dans la liste.
     task.remove();
 }
 
+function updateTaskName(task){  //fonction qui permet de modifier le texte par défaut d'une tâche dans la liste.
+    const newTextArea = task.querySelector('.task');
+    newTextArea.value = "Task "+ updateCounter() //tasksContainer.ariaValueNow;
+}
+
+function updateCounter(){   // fonction de calcul du compteur de tâche
+    return tasksContainer.children.length;
+}
